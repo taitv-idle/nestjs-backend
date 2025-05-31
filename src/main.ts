6d932f03c6b6d97794fb5e9d5432c0301e8da947
devtaitv <devtaitv@devtaitv.lan>
2025-05-31 14:28:58 +0700

    use class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { ConfigService } from '@nestjs/config'
-
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const configService = app.get(ConfigService);
-
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(configService.get<string>('PORT') ?? 3000);
 }
 bootstrap();
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,15 @@
+import { IsEmail, IsNotEmpty, Max, Min } from 'class-validator';
+
 export class CreateUserDto {
+  @IsEmail({}, { message: 'Email Không hợp lệ' })
+  @IsNotEmpty({ message: 'Email Không đc để trống' })
   email: string;
+
+  @IsNotEmpty({ message: 'Password Không đc để trống' })
+  @Min(8, { message: 'Password phải có ít nhất 8 ký tự' })
+  @Max(16, { message: 'Password không được quá 16 ký tự' })
   password: string;
+
+  @IsNotEmpty({ message: 'Name Không đc để trống' })
   name: string;
 }
